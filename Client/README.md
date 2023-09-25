50fcf2777c3c51862959f429bcf7d351eff30614
asez28 <114678077+asez28@users.noreply.github.com>
2023-09-25 23:37:33 +0300

    Delete Client directory

--- Client/README.md
+++
@@ -1,2 +0,0 @@
-# react-firebase.v.9-router.v.6.4-context.api-2023
-Este proyecto está en construcción. Revise las ramas para acceder al código actualizado.
--- Client/src/api/config.jsx
+++
@@ -1,3 +0,0 @@
-export const API_URL = `http://localhost:8080`;
-
-export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY5NTE1MjM4Mn0.f6fPzF8Zq-RNjQIWNSdgP3LhORIHI0tfMjLPenOrcKQ';
