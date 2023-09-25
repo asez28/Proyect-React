0b50a685eca2c66ae788a929137ee2edc34a4e28
asez28 <114678077+asez28@users.noreply.github.com>
2023-09-25 23:37:45 +0300

    Delete server directory

--- server/src/routes/deleteTodosRoute.js
+++
@@ -1,8 +0,0 @@
-const TodoModel = require('../models/TodoModel');
-
-module.exports = async (req, res) => {
-  const {id} = req.params;
-  const todos = await TodoModel.findById(id);
-  await todos.deleteOne();
-  res.status(204).json(todos);
-}
